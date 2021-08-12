2c7e47ba85f14d01db5a85d9775da899236ed823
Valentin Kladov <75556421+KladovValentin@users.noreply.github.com>
2021-08-12 21:45:05 +0700

    added description as comment

--- Room.h
+++ Room.h
@@ -2,6 +2,8 @@
 #include "MainHeader.h"
 #include "Wall.h"
 
+//has corners, links to walls (NULL if no wall yet), objects inside and neighbour rooms (to search position after frame change only inside nearby rooms)
+//convex is to search inside it, sfml just has usefull function "contains" - for example room contains hero.position
 class Room
 {
 public:
@@ -14,6 +16,7 @@ public:
 
     Room(vector<point> inputcorners);
     void linkWall(int borderIndex, Wall* wall);
+    //this linking is for "room[0]", "outside of a house"
     void linkWall0(Wall* wall);
     void addjust(Room* adjustedRoom);
 };
