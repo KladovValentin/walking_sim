ce8a38e7e6170d4162b020af775acab947391f06
Valentin Kladov <75556421+KladovValentin@users.noreply.github.com>
2021-08-12 22:00:54 +0700

    wrote descroptions of sume methods

--- Subject.h
+++ Subject.h
@@ -3,6 +3,7 @@
 #include "Wall.h"
 #include "functions_namespace.h"
 
+//movable by itself object
 class Subject :
     public Object
 {
@@ -12,10 +13,18 @@ public:
     Subject(point startCoord, double basespeed);
 
     point pos();
+    
+    //movement in derection with some speed (based on baseSpeed)
     void moves(point wheretogo, double movementMulty);
+    
+    //pushing in normal to line direction, so direction of lines is important! (clockwise around object)
     void pushFromLine(pair<point, point> linev, double distance);
+   
     double MinDistanceToObj(vector<pair<point, point>> linevarr);
+    
     void checkBlockByObject(vector<pair<point, point>> linevarr);
+    
+    //mostly create an array to put into checkBlockByObject
     void checkBlockByWall(Wall* wall, point movement);
 };
 
